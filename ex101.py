d28033ec32764eb52afdbd2e7174bd939b81bfe4
KhinYandarSoe <TinTinWin6789@gmail.com>
2018-06-26 14:12:23 +0630

    change cat again more

--- ex101.py
+++ ex101.py
@@ -1,15 +1,15 @@
-tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'am split\non aline."
-backslash_cat = "I'am \\ a \\ cat."
+tabby_rat = "\tI'm tabbed in."
+persian_rat = "I'am split\non aline."
+backslash_rat = "I'am \\ a \\ rat."
 
-fat_cat = """
+fat_rat = """
 I'll do a list:
-    \t* Cat food
+    \t* rat food
     \t* Fishies
     \t* Catnip\n\t* Grass
     """
 
-print(tabby_cat)
-print(persian_cat)
-print(backslash_cat)
-print(fat_cat)
+print(tabby_rat)
+print(persian_rat)
+print(backslash_rat)
+print(fat_rat)
