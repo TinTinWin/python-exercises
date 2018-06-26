3d18a127d1e696caa25017a0333d51eea82b7493
KhinYandarSoe <TinTinWin6789@gmail.com>
2018-06-26 14:24:25 +0630

    change cat text1

--- ex101.py
+++ ex101.py
@@ -4,11 +4,7 @@ backslash_cat = "I'am \\ a \\ cat."
 
 fat_cat = """
 I'll do a list:
-<<<<<<< HEAD
     \t* cat food
-=======
-    \t* Rat food
->>>>>>> 1af732950770cf1a66605ff13776d6b0dfb12065
     \t* Fishies
     \t* Catnip\n\t* Grass
     """
